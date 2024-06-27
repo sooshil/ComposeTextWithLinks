cdb824ea8677d2baebdc83d40449e7353f531ffe
sooshil <sushilkaf@gmail.com>
2024-06-27 16:22:23 -0400

    TextWithLinks implementation - complete

--- app/src/main/java/com/sukajee/composetextwithlinks/MainActivity.kt
+++ app/src/main/java/com/sukajee/composetextwithlinks/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
@@ -16,6 +18,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sukajee.composetextwithlinks.ui.theme.ComposeTextWithLinksTheme
 
@@ -33,40 +36,47 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize(),
                         verticalArrangement = Arrangement.Center
                     ) {
+                        Spacer(Modifier.height(32.dp))
                         TextWithLinks(
                             textStyle = TextStyle(
                                 fontSize = 24.sp,
                                 textAlign = TextAlign.Justify
                             ),
-                            fullText = "Welcome to our app! For more information about our Privacy Policy, " +
-                                "please review it carefully. If you're having trouble, check out " +
-                                "our Help Center for FAQs and troubleshooting guides. Ready to get started? " +
-                                "Head over to the Account Settings page to personalize your experience.",
-                            linksList = listOf(
-                                Link(
-                                    linkText = "Account Settings",
-                                    linkData = "https://www.example.com/account-settings",
-                                    style = SpanStyle(color = Color.Blue)
-                                ) { str -> // This str will return the linkData of the link.
-                                    Toast.makeText(
-                                        context,
-                                        "Clicked on $str", //Toast will show -> Clicked on https://www.example.com/account-settings
-                                        Toast.LENGTH_LONG
-                                    ).show()
-                                },
-                                Link(
-                                    linkText = "Help Center",
-                                    style = SpanStyle(color = Color.Red),
-                                    linkData = "refund"
-                                ) { str -> // This str will return the linkData of the link.
-                                    navigateToHelpCenterScreen(helpTopic = str)
-                                },
-                                Link(
-                                    linkText = "Privacy Policy",
-                                    style = SpanStyle(color = Color.Magenta)
-                                ) {
-                                    //handle click
-                                }
+                            linkData = LinkData(
+                                fullText = "Welcome to our app! For more information about our Privacy Policy, " +
+                                    "please review it carefully. If you're having trouble, check out " +
+                                    "our Help Center for FAQs and troubleshooting guides. Ready to get started? " +
+                                    "Head over to the Account Settings page to personalize your experience.",
+                                linksList = listOf(
+                                    Link(
+                                        linkText = "Account Settings",
+                                        linkInfo = "https://www.example.com/account-settings",
+                                        style = SpanStyle(color = Color.Blue)
+                                    ) { str -> // This str will return the linkInfo of the link.
+                                        Toast.makeText(
+                                            context,
+                                            "Clicked on $str", //Toast will show -> Clicked on https://www.example.com/account-settings
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    },
+                                    Link(
+                                        linkText = "Help Center",
+                                        style = SpanStyle(color = Color.Red),
+                                        linkInfo = "refund"
+                                    ) { str -> // This str will return the linkInfo of the link.
+                                        // navigateToHelpCenterScreen(helpTopic = str) will pass helpTopic = "refund"
+                                    },
+                                    Link(
+                                        linkText = "Privacy Policy",
+                                        style = SpanStyle(color = Color.Magenta),
+                                    ) {
+                                        Toast.makeText(
+                                            context,
+                                            "Clicked on Privacy Policy",
+                                            Toast.LENGTH_LONG
+                                        ).show()
+                                    }
+                                )
                             )
                         )
                     }
@@ -74,9 +84,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-private fun navigateToHelpCenterScreen(helpTopic: String) {
-    // Navigate to the Help Center screen with the helpTopic parameter
-
 }
--- app/src/main/java/com/sukajee/composetextwithlinks/TextWithLinks.kt
+++ app/src/main/java/com/sukajee/composetextwithlinks/TextWithLinks.kt
@@ -12,18 +12,29 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 
+/**
+ * Composable function to display text with clickable links.
+ *
+ * @param modifier [Modifier] to apply to the composable.
+ * @param linkData [LinkData] object containing the full text and list of links.
+ * @param textStyle [TextStyle] to apply to the text.
+ *
+ * This function creates an annotated string from the full text and links,
+ * and then uses a ClickableText composable to display the text and handle click events.
+ * When a link is clicked, the corresponding `onClick` function is invoked.
+ *
+ */
 @Composable
 fun TextWithLinks(
     modifier: Modifier = Modifier,
-    fullText: String,
-    textStyle: TextStyle = MaterialTheme.typography.titleSmall,
-    linksList: List<Link>,
+    linkData: LinkData,
+    textStyle: TextStyle = MaterialTheme.typography.bodyLarge,
 ) {
-    val annotatedString =
-        buildAnnotatedString {
-            append(fullText)
-            linksList.forEach { link ->
-                val startIndex = fullText.indexOf(link.linkText)
+    val annotatedString = buildAnnotatedString {
+        append(linkData.fullText)
+        linkData.linksList.forEach { link ->
+            var startIndex = linkData.fullText.indexOf(link.linkText)
+            while (startIndex >= 0) {
                 val endIndex = startIndex + link.linkText.length
                 addStyle(
                     style = link.style,
@@ -32,12 +43,14 @@ fun TextWithLinks(
                 )
                 addStringAnnotation(
                     tag = link.linkText,
-                    annotation = link.linkData ?: "",
+                    annotation = link.linkInfo ?: "",
                     start = startIndex,
                     end = endIndex
                 )
+                startIndex = linkData.fullText.indexOf(link.linkText, endIndex)
             }
         }
+    }
 
     ClickableText(
         modifier = modifier
@@ -46,7 +59,7 @@ fun TextWithLinks(
         text = annotatedString,
         style = textStyle,
         onClick = { position ->
-            linksList.forEach { link ->
+            linkData.linksList.forEach { link ->
                 annotatedString
                     .getStringAnnotations(link.linkText, position, position)
                     .firstOrNull()?.let {
@@ -58,10 +71,38 @@ fun TextWithLinks(
     )
 }
 
+/**
+ * Data class representing a link with its full text and a list of links.
+ *
+ * @property fullText The full text containing the link.
+ * @property linksList A list of [Link] objects representing the links found in the full text.
+ */
+@Immutable
+data class LinkData(
+    val fullText: String,
+    val linksList: List<Link>
+)
+
+/**
+ * Represents a link within the text, with a display text, optional link information, a style, and an
+ * action to perform when the link is clicked. Links can be used to navigate to other parts of the
+ * application, open external URLs, or trigger other actions.
+ *
+ * @property linkText The text to display for the link. This is the text that users will see and interact with.
+ * @property linkifyAllOccurrences Whether to linkify all occurrences of the `linkText` in the surrounding text.
+ * If `false`, only the first occurrence will be linked.
+ * @property linkInfo Optional additional information about the link. This can be used to provide more context
+ * or details about the link destination.
+ * @property style The style to apply to the link text. This can be used to customize the appearance of the link,
+ * such as the font, color, or underline.
+ * @property onClick The action to perform when the link is clicked. This can be used to navigate to another
+ * screen, open a URL, or trigger any other desired action.
+ */
 @Immutable
 data class Link(
     val linkText: String,
-    val linkData: String? = null,
+    val linkifyAllOccurrences: Boolean = false,
+    val linkInfo: String? = null,
     val style: SpanStyle,
     val onClick: (String) -> Unit
 )
